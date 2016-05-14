16a51dfbaa8e02972cf0d63ecda5ca0fa9c1566e
saurabhmaurya15 <saurabh.maurya999@gmail.com>
2016-05-14 11:20:03 +0530

    main.ts added

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+import { Http } from 'angular2/http';
+import { BaseService } from './common/base.service';
+
+@Component({
+	selector: 'main-component',
+	template: `<h1>AngularAttack 2016</h1><p>Your project runs!-Team</p>`,
+})
+export class AppComponent { 
+}
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import {bootstrap}    from 'angular2/platform/browser'
+import { AppComponent } from './app.component';
+import {HTTP_PROVIDERS, Http} from 'angular2/http'
+import {ROUTER_PROVIDERS, Router} from 'angular2/router';
+
+bootstrap(AppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS]);
